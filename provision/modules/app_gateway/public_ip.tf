1e7d5b17903b5659fe03ad3fd7cddb0c36f9cef3
dmakeroam <cpfapp@gmail.com>
2021-01-29 03:20:11 +0700

    - Updated app gateway sku to Standard_v2

--- provision/modules/app_gateway/public_ip.tf
+++ provision/modules/app_gateway/public_ip.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "aks_app_gateway_public_ip" {
   name                = "${var.company}_${var.project}_${var.environment}_${var.app_gw_name}_public_ip"
   resource_group_name = var.app_gw_resource_group_name
   location            = var.location
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
   ip_version          = "IPv4"
 }
